e6eb8391ff9bc0c4fe643137cb0154864309ecd8
xValentim <joaogvr@al.insper.edu.br>
2023-10-23 13:31:12 -0300

    add attach

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 def register_routes(app, root="api"):
     from app.members import register_routes as attach_users
     from app.plans import register_routes as attach_plans
+    from app.subscribe import register_routes as attach_subscribe
 
     attach_users(app)
     attach_plans(app)
+    attach_subscribe(app)
